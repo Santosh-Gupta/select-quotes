f991b77c3d521fb57c74656bbf43a8c4fa5b5873
Santosh Gupta <Santosh.gupta.eng@gmail.com>
2020-03-11 22:35:50 -0700

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -4,11 +4,13 @@ export const SELECT_QUOTES_COMMAND_ID = "select-quotes";
 
 export const DOUBLE_QUOTE = "\"";
 export const SINGLE_QUOTE = "'";
+export const LEFT_BRACKET = "(";
+export const RIGHT_BRACKET = ")";
 
 export const DEFAULT_POSITION:  Position  = new Position(0,0);
 export const DEFAULT_SELECTION: Selection = new Selection(DEFAULT_POSITION, DEFAULT_POSITION);
 
-export const isQuote = (c: string) => c === DOUBLE_QUOTE || c === SINGLE_QUOTE;
+export const isQuote = (c: string) => c === DOUBLE_QUOTE || c === SINGLE_QUOTE || c === LEFT_BRACKET || c === RIGHT_BRACKET; 
 
 export const isMouseEvent             = (e: TextEditorSelectionChangeEvent) => e.kind === TextEditorSelectionChangeKind.Mouse;
 export const isOnlyOneActiveSelection = (e: TextEditorSelectionChangeEvent) => e.selections.length === 1;
